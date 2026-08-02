52b06457d085c0ad12ce8c127b03413d660ac192
Krish Parmar <mystery4020@gmail.com>
2026-08-02 14:43:01 +0530

    Delete assets/js directory

--- assets/js/utils.js
+++
@@ -1,8 +0,0 @@
-/* Utility Functions */
-class Utils {
-    static formatDate() {
-        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-        return new Date().toLocaleDateString('en-US', options);
-    }
-}
-window.Utils = Utils;
