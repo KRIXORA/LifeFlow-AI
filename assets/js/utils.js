889e54edb6250d230c15fa580252f31d1786837c
Krish Parmar <mystery4020@gmail.com>
2026-08-02 11:25:34 +0530

    Remove generateId method from Utils class
    Removed the generateId method from the Utils class.

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -4,9 +4,5 @@ class Utils {
         const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
         return new Date().toLocaleDateString('en-US', options);
     }
-
-    static generateId() {
-        return '_' + Math.random().toString(36).substr(2, 9);
-    }
 }
 window.Utils = Utils;
